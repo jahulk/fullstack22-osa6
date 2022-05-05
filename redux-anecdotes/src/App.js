5600da0a6f44e8ee6b9c414fdc50f1b00fa715b5
Jarkko Hulkko <jarkko.hulkko@student.jamk.fi>
2022-05-05 13:07:14 +0300

    Poistin turhat importit

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,16 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm  from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (anecdote) => {
-    dispatch(voteAnecdote(anecdote))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
