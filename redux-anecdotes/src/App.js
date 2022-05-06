9f0fb310403af21ba6d9929d14d227401a2e44d1
Jarkko Hulkko <jarkko.hulkko@student.jamk.fi>
2022-05-06 11:04:45 +0300

    anekdootit ja backend, step2

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm  from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <Notification />
--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setMessage, removeMessage } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch()
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
     const content = event.target.content.value
     event.target.content.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setMessage(`new anecdote '${content}' created`))
     setTimeout(() => {
       dispatch(removeMessage())
